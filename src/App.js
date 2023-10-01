1fb9ee13f78287a446f30b8699b07a11d35ba402
Tomixer <tomix.klokar@gmail.com>
2023-10-01 21:08:10 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./index.css"
+import {useState,useEffect} from"react"
 
 const App=()=>{
-  return  <h1>test</h1>
+
+  useEffect((()=>{
+    console.log("ja jsem useEffect")
+  }))
+
+  return  <div>
+  <h1>test</h1>
+  {console.log("ja jsem return")}
+  </div>
 }
 export default App
