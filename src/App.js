e7025c11ab5ed44fa5db68c67ff8a0c6a4a5f5dd
Tomixer <tomix.klokar@gmail.com>
2023-10-02 18:38:53 +0200

    pridanie console.log

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./index.css"
 import {useState,useEffect} from"react"
 
 const App=()=>{
+  console.log("uplne hore")
   const [vlaue,setvalue]=useState(1)
   useEffect((()=>{
     console.log("ja jsem useEffect")
   }))
-
+  console.log("text pred return")
   return  <div>
   <h1>test</h1>
   <p>{vlaue}</p>
