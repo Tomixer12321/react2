ace4065579814f45532a3ce36164777f6c75333d
Tomixer <tomix.klokar@gmail.com>
2023-10-02 18:35:02 +0200

    pridal som button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./index.css"
 import {useState,useEffect} from"react"
 
 const App=()=>{
-
+  const [vlaue,setvalue]=useState(1)
   useEffect((()=>{
     console.log("ja jsem useEffect")
   }))
 
   return  <div>
   <h1>test</h1>
+  <p>{vlaue}</p>
   {console.log("ja jsem return")}
+  <button onClick={()=>setvalue(vlaue+1)}>klikni</button>
   </div>
 }
 export default App
