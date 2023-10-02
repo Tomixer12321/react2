c3201a1258b5165dff034b993c643fd12f4703f6
Tomixer <tomix.klokar@gmail.com>
2023-10-02 21:19:49 +0200

    pridal som zmenu na titulok

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [value, setvalue] = useState(0);
+  const [test, setTest] = useState(0);
+
   useEffect(() => {
     const button = document.querySelector(".btn");
     if (value > 0) {
       button.textContent = `klick colso ${value}`;
-    }else{
-      button.textContent="klick"
+    } else {
+      button.textContent = "klick";
     }
-  });
+  }, [value]);
+
+  useEffect(() => {
+    document.title = `novy titulek ${test}`;
+  }, [test]);
+
   return (
     <div>
       <h1>test</h1>
       <p>{value}</p>
-      {console.log("ja jsem return")}
       <button className="btn" onClick={() => setvalue(value + 1)}>
         klikni
       </button>
+      <button onClick={() => setTest(test + 1)}>titulek</button>
     </div>
   );
 };
