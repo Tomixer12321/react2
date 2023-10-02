0461d4d929043c0598b01f3504fa791a92b5efc2
Tomixer <tomix.klokar@gmail.com>
2023-10-02 18:47:45 +0200

    upravy a zmena tlacitka

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import "./index.css"
-import {useState,useEffect} from"react"
+import "./index.css";
+import { useState, useEffect } from "react";
 
-const App=()=>{
-  console.log("uplne hore")
-  const [vlaue,setvalue]=useState(1)
-  useEffect((()=>{
-    console.log("ja jsem useEffect")
-  }))
-  console.log("text pred return")
-  return  <div>
-  <h1>test</h1>
-  <p>{vlaue}</p>
-  {console.log("ja jsem return")}
-  <button onClick={()=>setvalue(vlaue+1)}>klikni</button>
-  </div>
-}
-export default App
+const App = () => {
+  const [value, setvalue] = useState(1);
+  useEffect(() => {
+    const button = document.querySelector(".btn");
+    button.textContent=`klick colso ${value}`
+  });
+  return (
+    <div>
+      <h1>test</h1>
+      <p>{value}</p>
+      {console.log("ja jsem return")}
+      <button className="btn" onClick={() => setvalue(value + 1)}>
+        klikni
+      </button>
+    </div>
+  );
+};
+export default App;
