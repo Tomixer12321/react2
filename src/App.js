97b689cef9a70535ad6926e8dce4d63f7a26e34a
Tomixer <tomix.klokar@gmail.com>
2023-10-03 20:09:35 +0200

    sirka okna

--- src/App.js
+++ src/App.js
@@ -2,30 +2,17 @@ import "./index.css";
 import { useState, useEffect } from "react";
 
 const App = () => {
-  const [value, setvalue] = useState(0);
-  const [test, setTest] = useState(0);
-
-  useEffect(() => {
-    const button = document.querySelector(".btn");
-    if (value > 0) {
-      button.textContent = `klick colso ${value}`;
-    } else {
-      button.textContent = "klick";
-    }
-  }, [value]);
-
-  useEffect(() => {
-    document.title = `novy titulek ${test}`;
-  }, [test]);
-
+  const [windowSize,setWindowSize]=useState(window.innerWidth)
+  const sizecontrol=()=>{
+    setWindowSize(window.innerWidth)
+  }
+  useEffect(()=>{
+    window.addEventListener("resize",sizecontrol)
+  })
   return (
     <div>
-      <h1>test</h1>
-      <p>{value}</p>
-      <button className="btn" onClick={() => setvalue(value + 1)}>
-        klikni
-      </button>
-      <button onClick={() => setTest(test + 1)}>titulek</button>
+      <h1>sirka okna</h1>
+      <h2>{windowSize}</h2>
     </div>
   );
 };
