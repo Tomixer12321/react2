77d2b1a6be11bb7598f658ba59eb04e459330e9b
Tomixer <tomix.klokar@gmail.com>
2023-10-05 21:48:58 +0200

    dokoncenie trackovanie iss

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ const App = () => {
   const url = "http://api.open-notify.org/iss-now.json";
   const [latitude, setlatitude] = useState("");
   const [longitude, setlongitude] = useState("");
+  const [urlMap, setUrlMap] = useState("");
 
   const getCoordinates = async () => {
     const responst = await fetch(url);
     const data = await responst.json();
-    setlatitude(data["iss_position"]["latitude"])
-    setlongitude(data["iss_position"]["longitude"])
+    setlatitude(data["iss_position"]["latitude"]);
+    setlongitude(data["iss_position"]["longitude"]);
+
+    const iss_long =data["iss_position"]["longitude"]
+    const iss_lat =data["iss_position"]["latitude"]
+
+    setUrlMap(`https://sk.mapy.cz/zakladni?x=${iss_long}&y=${iss_lat}&z=5`);
   };
-  useEffect(()=>{
+  useEffect(() => {
     getCoordinates();
-  },[])
+  }, []);
 
   return (
     <div>
@@ -22,6 +28,7 @@ const App = () => {
       <p>{latitude}</p>
       <h1>zemepisna delka</h1>
       <p>{longitude}</p>
+      <a href={urlMap} target="_blank">odkaz od map</a>
     </div>
   );
 };
