19945c94349c4e67e63c79785c99283598d6fed5
Tomixer <tomix.klokar@gmail.com>
2023-10-06 22:02:14 +0200

    funkcia na skrytie a ukazanie

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import { useState, useEffect } from "react";
-const url = "http://api.open-notify.org/iss-now.json";
-
+import Book from "./components/Book";
+import {useState}from "react"
 
 const App = () => {
-  const [loading, setloading] = useState(false);
-  const [latitude,setlatitude]=useState("")
-  const [longitude,setlongitude]=useState("")
-
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => data["iss_position"])
-      .then((position)=>{
-        setlatitude(position["latitude"])
-        setlongitude(position["longitude"])
-      })
-  }, []);
-
-  if(loading){
-    return <h1>nacitani stranky...</h1>;
-  }
+  const [show,setShow]=useState(false)
     return <div>
-      <h2>zemepisna siska</h2>
-      <p>{latitude}</p>
-      <h2>zemiepisna delka</h2>
-      <p>{longitude}</p>
+      <button onClick={()=>setShow(!show)}>ukaz/skryj</button>
+    {show && <Book />}
     </div>
   
 };
